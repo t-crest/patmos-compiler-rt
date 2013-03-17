1adea41e13b500630d2068d12aa556be3d5e06b7
Stefan Hepp <stefan@stefant.org>
2013-03-17 02:42:06 +0100

    Add llvm.used attributes to syms files

--- lib/patmos/markused.sh
+++ lib/patmos/markused.sh
@@ -7,6 +7,10 @@ if [ ! -f $LL_FILE ]; then
   error_exit 1 "Bitcode file $LL_FILE does not exist!"
 fi
 
+OUT_FILE=$2
+if [ "$2" == "" ]; then
+    OUT_FILE=$LL_FILE
+fi
 
 function error_exit {
   local errno=$1; shift
@@ -15,7 +19,7 @@ function error_exit {
 }
 
 
-echo "Adding llvm.used to $LL_FILE"
+echo "Adding llvm.used to $OUT_FILE"
 
 cnt=0
 LLVM_USED=
@@ -23,19 +27,20 @@ LLVM_USED=
 IFS=$'\n'
 for fn in $(grep "declare" $LL_FILE); do
     if [ "x$LLVM_USED" != "x" ]; then
-	LLVM_USED="$LLVM_USED,"
+	LLVM_USED="$LLVM_USED,\n"
     fi
     # Extract signature, remove variable names of arguments
-    SIG=$(echo $fn | sed "s/declare \([a-z0-9*]*\) \(@[a-zA-Z0-9_]*\)\((.*)\).*/\1 \3* \2/" | sed "s/ %[a-zA-Z_]*//g")
-    LLVM_USED="$LLVM_USED i8* bitcast ($SIG to i8*)"
+    SIG=$(echo $fn | sed "s/declare \([a-z0-9*]*\) *\(@[a-zA-Z0-9_]*\)\((.*)\).*/\1 \3* \2/" | sed "s/\( nocapture\)* %[a-zA-Z_]*//g")
+    LLVM_USED="$LLVM_USED  i8* bitcast ($SIG to i8*)"
     cnt=$((cnt+1))
 done
 
-echo >> $LL_FILE
-echo "@llvm.used = appending global [$cnt x i8*] [" >> $LL_FILE 
-echo "  $LLVM_USED" >> $LL_FILE
-echo '], section "llvm.metadata"' >> $LL_FILE
-
-echo Done.
+if [ "$OUT_FILE" != "$LL_FILE" ]; then
+    cat $LL_FILE > $OUT_FILE
+fi
+echo >> $OUT_FILE
+echo "@llvm.used = appending global [$cnt x i8*] [" >> $OUT_FILE
+echo -e "$LLVM_USED" >> $OUT_FILE
+echo '], section "llvm.metadata"' >> $OUT_FILE
 
 exit 0
